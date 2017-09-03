fa1a0a62597eb51457f8c105f995e1af66aa0709
ptm213 <30703473+ptm213@users.noreply.github.com>
2017-09-03 14:22:05 -0400

    Answered Q8

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,19 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+# Change directory
+import os
+os.chdir('/Users/PaulM/ds/metis/metisgh/prework/dsp/python')
+os.getcwd()
+
+# Import and format data
+football = open('football.csv').read() 
+data = football.split('\n')
+data = [x.split(',') for x in data] # List of football data lists
+
+# Calculate new variable and sort
+goal_diff = [[x[0], abs(int(x[5])- int(x[6]))] for x in data[1:]] # Relevant information only
+ascending_teams = sorted(goal_diff, key = lambda x: x[1]) # Sort teams by goal differential
+least_diff = ascending_teams[0][0] # Team w/ smallest goal differential
+
+print("The team with the smallest goal differential is {}".format(least_diff))
